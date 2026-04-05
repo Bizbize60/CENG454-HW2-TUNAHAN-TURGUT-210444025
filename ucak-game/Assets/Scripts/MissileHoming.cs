8ee2ef8a281b99e794d69054c26a848623b31f34
Bizbize60 <s210444025@stu.thk.edu.tr>
2026-04-05 20:15:40 +0300

    feat: add rotate function

--- ucak-game/Assets/Scripts/MissileHoming.cs
+++ ucak-game/Assets/Scripts/MissileHoming.cs
@@ -16,11 +16,21 @@ public class MissileHoming : MonoBehaviour
     { 
         // TODO (Task 3-F): rotate toward the target and move forward 
 	moveToJet();
-
+	rotateToJet();
     } 
 
     void moveToJet()
     {
-     transform.position =  Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * moveSpeed)
+     transform.position =  Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * moveSpeed);
     }
+
+   void rotateToJet()  //StackOverFlow fix usage for rotation 
+   {
+     
+     Vector3 direction = target.position - transform.position;
+     if (direction != Vector3.zero) {
+     Quaternion rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * turnSpeed);
+     transform.rotation = rotation;
+     }
+   }
 } 
